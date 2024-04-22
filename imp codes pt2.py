1a53d34a4c240a9c3650f24d9c5b205313d95434
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-04-22 22:42:01 +0530

    Update imp codes pt2.py

--- imp codes pt2.py
+++ imp codes pt2.py
@@ -1,1 +1,91 @@
+//image animations 
+components.html(
+    """
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0; /* Remove default margin */
+  padding: 0; /* Remove default padding */
+}
+body {
+  font-family: Verdana, sans-serif;
+}
+.mySlides {
+  display: none;
+}
+img {
+  vertical-align: middle;
+  width: 100%; /* Make images fill their containers */
+  margin: 0; /* Remove any margin */
+  padding: 0; /* Remove any padding */
+}
+/* Slideshow container */
+.slideshow-container {
+  max-width: 300px;
+  max-height : 300px;
+  position: 100%;
+  margin: 0;
+}
 
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+</style>
+</head>
+<body>
+<div class="slideshow-container">
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920">
+    
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920">
+    
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920">
+    
+  </div>
+</div>
+<script>
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+</script>
+</body>
+</html>
+    """,
+    height=200, width=300
+)
